c82c5251f9b3859c5b01092b6080129576fa751d
Ben Sutherland <blender.sleuth@gmail.com>
2018-11-21 15:37:42 +1100

    radial gravity works nicely

--- player/Player.gd
+++ player/Player.gd
@@ -1,43 +1,95 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-const GRAV = 100000
-const MOVE_SPEED = 2
-const JUMP_SPEED = 20
+export (int) var gravity = 1200
 
-export (Vector2) var planet = Vector2();
+export (int) var run_speed = 100
+export (int) var fly_speed = 20
+export (int) var max_run_speed = 500
+export (float) var run_damp = 0.85
+export (float) var fly_damp = 0.98
 
-var on_ground = false
+export (int) var jump_speed = 100
+export (float) var jump_time = 0.24 # Time in seconds for advanced control over jump
 
-onready var arrow = $arrow
+export (NodePath) var planetPath
+onready var planet = get_node(planetPath)
 
-var gravVec = Vector2()
-
+# Velocity is relative to node, unrotated
+var velocity = Vector2()
+var jumping = false
 
 func _ready():
-    pass
+	pass
 
-func _integrate_forces(state):
-    gravVec = (planet - position).normalized()
-    
-    var global_rotation_angle = Vector2(0, -1).angle_to(gravVec)
-    arrow.global_rotation = global_rotation_angle
-    
-    var cur_dir = Vector2(0, 1).rotated(global_rotation)
-    var rotation_angle = cur_dir.angle_to(gravVec)
-    state.angular_velocity = (rotation_angle / state.get_step())
-    
-    var gravforce = gravVec * GRAV / position.distance_squared_to(planet)
-    state.linear_velocity += gravforce
-    
-    var moveVec = Vector2(MOVE_SPEED, 0).rotated(rotation)
-    
-    if Input.is_action_pressed("ui_left"):
-        state.linear_velocity -= moveVec
-    if Input.is_action_pressed("ui_right"):
-        state.linear_velocity += moveVec
-        
+var timer = 0.0
+func jump(delta):
+	var newjump = Input.is_action_just_pressed('ui_select')
+	
+	# Only reset if it is a new jump, and from the floor
+	if newjump and not jumping and is_on_floor():
+		jumping = true
+		velocity.y = 0
+		timer = 0.0
+	
+	# More of the current jump
+	if jumping and timer < jump_time:
+		var proportion_completed = timer / jump_time
+		var jump_vec = Vector2(0, -jump_speed).linear_interpolate(Vector2(0, 0), proportion_completed)
+		velocity += jump_vec
+		timer += delta
+	else:
+		# Finished jumping
+		jumping = false
 
-func _process(delta):
-    
-    if Input.is_action_pressed("ui_jump"):
-        apply_impulse(position, Vector2(0, -JUMP_SPEED).rotated(rotation))
+func get_input(delta):
+	var right = Input.is_action_pressed('ui_right')
+	var left = Input.is_action_pressed('ui_left')
+	var jump = Input.is_action_pressed('ui_select')
+
+	# Slow down velocity 
+	velocity.x *= run_damp if is_on_floor() else fly_damp
+
+	if jump:
+		jump(delta)
+	else:
+		jumping = false
+	
+	# Different speed on ground and in air
+	var move_speed = run_speed if is_on_floor() else fly_speed
+	if right:
+		velocity.x += move_speed
+	if left:
+		velocity.x -= move_speed
+	
+	# Make sure player doesn't accelerate too much
+	velocity.x = clamp(velocity.x, -max_run_speed, max_run_speed)
+
+func _physics_process(delta):
+	# Player movement and jumps
+	get_input(delta)
+	
+	# Apply gravity
+	velocity.y += gravity * delta
+	
+	# Normalised vector in direction of gravity
+	var gravVec = (planet.position - position).normalized()
+	
+	# Rotation to point towards the gravitational force
+	var rotation_angle = Vector2(0, 1).angle_to(gravVec)
+	# Rotate the kinematic body so the bottom faces the force
+	rotation = rotation_angle
+	
+	# Velocity in global coordinates
+	var global_velocity = velocity.rotated(rotation)
+	
+	# Floor is relative to player orientation
+	var floor_normal = Vector2(0, -1).rotated(rotation)
+	# Move player based on velocity
+	global_velocity = move_and_slide(global_velocity, floor_normal)
+	# Local velocity
+	velocity = global_velocity.rotated(-rotation)
+	
+	# If player collided with something
+	if get_slide_count() > 0:
+		var collision = get_slide_collision(0)
+		jumping = false
--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,9 +1,4 @@
 extends Node2D
 
-onready var background = $Background
-
 func _ready():
-    pass
-
-func _process(delta):
-    background.rect_size = get_viewport_rect().size
+	pass
