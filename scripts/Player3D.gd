aea27fe97c17dae27ec5e53318569ad5bbaf40ad
Ben <blender.sleuth@gmail.com>
2019-01-04 09:45:03 +1100

    Impossible gravity almost working, not quite though

--- scripts/Player3D.gd
+++ scripts/Player3D.gd
@@ -19,6 +19,7 @@ var planet_position = Vector3()
 # Node References
 onready var player_camera = $"../PlayerCamera"
 onready var front_camera = $"../FrontCamera"
+onready var ground_ray = $GroundRay
 
 var cam_offset = Vector3()
 
@@ -40,6 +41,9 @@ func _process(delta):
 			player_camera.make_current()
 			
 	player_camera.transform.origin = transform.origin + cam_offset
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		grav = !grav
 
 
 var jumping = false
@@ -99,10 +103,15 @@ func get_movement(delta):
 	
 	return move
 
+var grav = true
 
 func _physics_process(delta):
 	# Vector in the direction of gravity
-	var grav_vec = (planet_position - transform.origin).normalized()
+	var grav_vec = Vector3()
+	if ground_ray.is_colliding():
+		grav_vec = -ground_ray.get_collision_normal()
+	else:
+		grav_vec = (planet_position - transform.origin).normalized()
 	
 	# Feet of player
 	var down = -transform.basis.y
@@ -117,14 +126,14 @@ func _physics_process(delta):
 	else:
 		rot_axis = rot_axis.normalized()
 	
-	# Find the angle to rotate
-	var dot = clamp(down.dot(grav_vec), -1, 1)
-	# Both are unit vectors, so dot product = cos(angle)
-	var angle = acos(dot)
-	
-	# Rotate player so the local down direction is in the direction of gravity
-	transform.basis = transform.basis.rotated(rot_axis, angle)
-	transform = transform.orthonormalized() # Fix up basis
+		# Find the angle to rotate
+		var dot = clamp(down.dot(grav_vec), -1, 1)
+		# Both are unit vectors, so dot product = cos(angle)
+		var angle = acos(dot)
+		
+		# Rotate player so the local down direction is in the direction of gravity
+		transform.basis = transform.basis.rotated(rot_axis, angle)
+		transform = transform.orthonormalized() # Fix up basis
 	
 	# Apply input movement
 	var movement = get_movement(delta)
@@ -132,7 +141,10 @@ func _physics_process(delta):
 	velocity += transform.basis.xform(movement)
 	
 	# Add gravity 
-	velocity += grav_vec * gravity
+	velocity += grav_vec * gravity if grav else Vector3()
+	
+	# Clamp velocity length to something reasonable
+#	velocity = velocity.normalized() * clamp(velocity.length(), -15, 15)
 	
 	# Apply friction or air resistance
 	var damp = run_damp if is_on_floor() else fly_damp
@@ -145,7 +157,39 @@ func _physics_process(delta):
 		# Sanity check
 		if !is_on_floor():
 			print("why")
-			assert(false)
+#			assert(false)
 
 
-
+#
+#func move_and_slide_custom(var lv, var floor_direction = Vector3(0,1,0), 
+#						var physics_delta = get_physics_process_delta_time(), 
+#						var max_slides = 4, 
+#						var slope_stop_min_velocity = 0.05, 
+#						var floor_max_angle = deg2rad(45)):
+#	var motion = (floor_velocity + lv) * physics_delta
+#	floor_velocity = Vector3(0,0,0)
+#	on_floor = false
+#	while(max_slides):
+#		var collision = move_and_collide(motion)
+#		if collision:
+#			motion = collision.remainder
+#			on_floor = true
+#			if (collision.normal.dot(floor_direction) >= cos(floor_max_angle)):
+#				floor_velocity = collision.collider_velocity
+#				var rel_v = lv - floor_velocity
+#				var hor_v = rel_v - floor_direction * floor_direction.dot(rel_v)
+#				if collision.get_travel().length() < 0.05 and hor_v.length() < slope_stop_min_velocity:
+#					var gt = get_global_transform()
+#					gt.origin -= collision.travel 
+#					set_global_transform(gt)
+#					return (floor_velocity - floor_direction * floor_direction.dot(floor_velocity))
+#			var n = collision.normal
+#			motion = motion.slide(n)
+#			lv = lv.slide(n)
+#		else:
+##	        print("no collision")
+#			break
+#		max_slides -= 1
+#		if motion.length() == 0:
+#			break
+#	return lv
